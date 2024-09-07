07a963047d44241b35c666df3989e01551c2c334
riverar9 <rtk0211@gmail.com>
2024-09-07 12:03:51 -0400

    completed assignment 1

--- data602-advanced-programming-techniques/assignments/assignment_01-richie-rivera.py
+++ data602-advanced-programming-techniques/assignments/assignment_01-richie-rivera.py
@@ -7,27 +7,52 @@
 HIGH_SCORE = 95
  
 # Get the test scores.
-test1 = input('Enter the score for test 1: ')
-test2 = input('Enter the score for test 2: ')
+test1 = int(input('Enter the score for test 1:\n\t')) # RR: Converted this into an integer
+test2 = int(input('Enter the score for test 2:\n\t')) # RR: Converted this into an integer
+test3 = int(input('Enter the score for test 3:\n\t')) # RR: This line was missing in the collection of test Scores. Converted this into an integer
 # Calculate the average test score.
-average = test1 + test2 + test3 / 3
+average = (test1 + test2 + test3) / 3 # RR: Added the brackets to perform the sum operation before the division operation.
 # Print the average.
-print('The average score is', average)
+print(f'The average score is {average}') # RR: Changed this line to be more readable
 # If the average is a high score,
 # congratulate the user.
-if average >= high_score:
+if average >= HIGH_SCORE: # RR: Updated variable name to reflect the previous high hscore
     print('Congratulations!')
 print('That is a great average!')
 
 #Q2
-#The area of a rectangle is the rectangle’s length times its width. Write a program that asks for the length and width of two rectangles and prints to the user the area of both rectangles. 
+#The area of a rectangle is the rectangle’s length times its width. Write a program that asks for 
+# the length and width of two rectangles and prints to the user the area of both rectangles. 
 
+def get_rectangular_area(length, width):
+    return length * width
+
+def input_int(input_text):
+    response = int(input(input_text + '\n\t'))
+
+    if type(response) == int:
+        return response
+    else:
+        print('That was not a valid response. Please, try again.')
+        return input_int(input_text)
+
+def get_user_rectangle_input_and_area(rectangle_id):
+    length = input_int(f"What is rectangle {rectangle_id}'s length?")
+    width = input_int(f"What is rectangle  {rectangle_id}'s width?")
+
+    print(f"Rectangle {rectangle_id}'s area is : {length * width}")
+
+for each_number in range(0,2):
+    get_user_rectangle_input_and_area(each_number)
 
 #Q3 
 #Ask a user to enter their first name and their age and assign it to the variables name and age. 
-#The variable name should be a string and the variable age should be an int.  
+#The variable name should be a string and the variable age should be an int.
+
+name = input('What is your name?\n\t')
+age = input_int('What is your age?\n\t')
 
 #Using the variables name and age, print a message to the user stating something along the lines of:
 # "Happy birthday, name!  You are age years old today!"
 
-
+print(f"Happy Birthday {name}! You are {age} years old today!")
