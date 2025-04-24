ef1a31b9647c3005665d2f0526b29ce08ba3b047
Richie Rivera <richie@riveradc.org>
2025-04-24 18:52:42 -0400

    data608 uploaded data and initialized viz script

--- data608-knowledge-and-visual-analytics/stories/story-6/create_viz.py
+++ data608-knowledge-and-visual-analytics/stories/story-6/create_viz.py
@@ -1,1 +1,9 @@
 # %%
+import pandas as pd
+# %%
+df = pd.read_csv(r'data/foodsecurity_state_2023.csv', sep=',', encoding='windows-1252')
+df = df.loc[df['Year'] == '2021–2023']
+df = df.loc[df['State'] != 'U.S.']
+
+df.head()
+# %%
